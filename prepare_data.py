8e8beb9872d8f8254531946a0c2d859918af4e51
Supratik Saha <totoslg@gmail.com>
2020-08-23 12:54:39 -0700

    feat: Import commonly used function from utils

--- prepare_data.py
+++ prepare_data.py
@@ -12,7 +12,7 @@ from multiprocessing import Pool  # Multiprocess Runs
 from typing import Any, Callable, Dict, List
 from sklearn.decomposition import PCA
 from functools import partial
-from utils import data_path, save_data_path, make_normal_lag
+from utils import data_path, save_data_path, make_normal_lag, reduce_mem_usage
 
 
 def sizeof_fmt(num: float, suffix: str = 'B') -> str:
@@ -29,45 +29,6 @@ def sizeof_fmt(num: float, suffix: str = 'B') -> str:
         num /= 1024.0
 
     return "%.1f%s%s" % (num, 'Yi', suffix)
-
-
-def reduce_mem_usage(df: pd.DataFrame, verbose: bool = True) -> pd.DataFrame:
-    """ Function that helps scale down memory usage of a data frame
-    Args:
-        df: Input data frame
-        verbose: Boolean value when set to True prints memory usage reduction
-    Returns:
-        Data Frame with reduced memory usage
-    """
-    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-    start_mem = df.memory_usage().sum() / 1024 ** 2
-    for column in df.columns:
-        col_type = df[column].dtypes
-        if col_type in numerics:
-            c_min = df[column].min()
-            c_max = df[column].max()
-            if str(col_type)[:3] == 'int':
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[column] = df[column].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[column] = df[column].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[column] = df[column].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[column] = df[column].astype(np.int64)
-            else:
-                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-                    df[column] = df[column].astype(np.float16)
-                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-                    df[column] = df[column].astype(np.float32)
-                else:
-                    df[column] = df[column].astype(np.float64)
-    end_mem = df.memory_usage().sum() / 1024 ** 2
-    if verbose:
-        print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(
-            end_mem, 100 * (start_mem - end_mem) / start_mem))
-
-    return df
 
 
 def merge_by_concat(df1: pd.DataFrame, df2: pd.DataFrame, merge_on: List[str]) -> pd.DataFrame:

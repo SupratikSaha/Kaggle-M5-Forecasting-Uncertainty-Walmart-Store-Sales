cb103f62398afd2bd4bcab9640d18c190dcae089
Supratik Saha <totoslg@gmail.com>
2020-08-23 12:55:35 -0700

    docs: Modify code file doc string

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-""" main file to run solution """
+""" Main file to create submission for M5 Uncertainty Forcasting Competition """
 
 from prepare_data import prepare_datasets
 from train import train_model

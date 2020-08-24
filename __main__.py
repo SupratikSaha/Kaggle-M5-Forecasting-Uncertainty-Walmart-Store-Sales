13b37ddc8ce3cd69abde51bfa17428a49d48519d
Supratik Saha <totoslg@gmail.com>
2020-08-23 17:13:35 -0700

    feat: Add prediction functions to main file

--- __main__.py
+++ __main__.py
@@ -2,6 +2,10 @@
 
 from prepare_data import prepare_datasets
 from train import train_model
+from predict import predict_results
+from predict_uncertainty import predict_uncertainties
 
 prepare_datasets()
 train_model()
+predict_results()
+predict_uncertainties()

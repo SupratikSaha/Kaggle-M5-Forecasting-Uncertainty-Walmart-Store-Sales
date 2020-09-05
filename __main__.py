190b496f5caeca820db5337824816c79448d33bf
Supratik Saha <totoslg@gmail.com>
2020-09-05 09:46:43 -0700

    feat: Add prediction functions to main file

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 """ Main file to create submission for M5 Uncertainty Forecasting Competition """
 
-from prepare_data import prepare_datasets
+from pre_process import prepare_datasets
 from train import train_model
 from predict import predict_results
-from predict_uncertainty import predict_uncertainties
+from post_process import predict_uncertainties
 
 prepare_datasets()
 train_model()
